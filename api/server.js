dec6529f7a08d6eb299ce7e6d9902c33f60a14eb
Ivan Beltran <ivanbeltran1704@gmail.com>
2024-05-23 20:08:44 -0600

    se cambian configuraciones

--- api/server.js
+++ api/server.js
@@ -1,18 +1,24 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
 
-server.use(middlewares)
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/product/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+ console.log("JSON Server is running");
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
