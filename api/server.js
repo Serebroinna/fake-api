50c7a624a2e7fdd36b7c55f97c28bc46336726c4
Ivan Beltran <ivanbeltran1704@gmail.com>
2024-07-16 21:32:42 -0600

    feat:

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const router = jsonServer.router("db.json");
 
 
 server.use(cors({
-   origin: "http://localhost:5173/"
+   origin: "http://localhost:5173"
 }));
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
